e13b105dcd17da8fe5d33d1b99293af39f2d7e41
Brandon Barrette <brandon.barrette@gmail.com>
2015-02-06 15:37:23 -0800

    Added basic lesson on strings for Monday

---
+++ ruby/lessons/1-string_sample.rb
@@ -0,0 +1,21 @@
+# Print the string Hello World to the screen.
+puts "hello world"
+
+# What's the difference between print and puts?
+print "Hello World"
+print "Hello World"
+print "Hello World\n"
+
+
+# Strings
+
+
+
+# Make a string in that has quotes in the string
+my_string = 'My favorite quote is: "Hello World"'
+
+# Make a string that has an apostrophe in the string
+my_other_string = "Mr. Barrette's Day"
+
+# Make a string that has BOTH
+yet_another_string = 'I heard someone say: "How\'s your day?"'
