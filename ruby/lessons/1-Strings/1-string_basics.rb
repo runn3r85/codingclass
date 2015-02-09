81c01652fe6ae88b21bd1fa157f7d1c53aa4950a
Brandon Barrette <brandon.barrette@gmail.com>
2015-02-09 15:42:55 -0800

    added first day completed lessons

--- ruby/lessons/1-Strings/1-string_basics.rb
+++ ruby/lessons/1-Strings/1-string_basics.rb
@@ -1,21 +1,40 @@
 # Print the string Hello World to the screen.
 
+puts "Hello World!" 
+puts "------------"
 # What's the difference between print and puts?
-
-
+print "Hello World!\n"
+print "Hello World!\n"
+print "Hello World!\n"
+puts "------------"
+# Use puts when you want the output on its own line
+# Use print when you want the output on the same line
 
 ###### Strings #######
 
-# create a variable 'name' and assign it to a string with your name 
+# Create a variable 'name' and assign it 
+# to a string with your name 
 
+name = "Mr. Barrette"
+puts name
 
 # Make a string that has quotes in the string
-my_string = 
+my_string = '"Welcome Back"'
+puts my_string
 
 # Make a string that has an apostrophe in the string
-my_other_string = 
+my_other_string = "Mr. Barrette's Awesome"
+puts my_other_string
 
-# Make a string that has BOTH
-yet_another_string = 
+# Make a string that has BOTH (escape the apostrophe)
+yet_another_string = 'I heard someone say: "How\'s your day?"'
+puts yet_another_string
 
 # Make a string that has a variable inside.
+hello = "Well Hello"
+message = "#{hello}, #{name}, hope you have a good day!"
+puts message
+
+
+
+
--- ruby/lessons/1-Strings/2-string_whitespace.rb
+++ ruby/lessons/1-Strings/2-string_whitespace.rb
@@ -1,13 +1,13 @@
-# Special characters:
-#  newline 
-#  tab 
-#  space
+# Special characters for Whitespace:
+# \n newline 
+# \t tab 
+# \s space
 
 # Use a newline in a string:
-
-
+puts "This\nIs\nA\nhorrible\nway\nto\nwrite\nA\nsentence\n"
 
 # Use a tab in a string:
-
+puts "\tFour scores and seven years ago,\nour forefathers..."
 
 # Use a space in a string:
+puts "This\sis\sa\shorrible\sway\sto\swrite\sA\ssentence\s"
