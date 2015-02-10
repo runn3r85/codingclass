1f535b0536867951922f34488037dc535b479d21
Brandon Barrette <brandon.barrette@gmail.com>
2015-02-10 14:19:24 -0800

    finish ruby strings lessons

--- ruby/lessons/1-Strings/3-string_methods.rb
+++ ruby/lessons/1-Strings/3-string_methods.rb
@@ -1,10 +1,20 @@
+# Methods (Functions)
+# Methods are applied to a string with
+# string.method 
+
 # Create a string variable with your name:
-name = 
+name = "Mr. Barrette"
+puts "My name is #{name}"
 
 # Uppercase a string:
+puts "My uppercased name is #{name.upcase}"
 
+# name.upcase! is equivalent to:
+# name = name.upcase
 
 # Turn a string in to lowercase:
-
+puts "My lowercased name is #{name.downcase}"
 
 # Reverse a string:
+puts "My reversed name is #{name.reverse}"
+
--- ruby/lessons/1-Strings/4-adding_strings.rb
+++ ruby/lessons/1-Strings/4-adding_strings.rb
@@ -1,20 +1,20 @@
 # Adding Strings
 
 # Define your names
-first_name = 
-last_name = 
+first_name = "Mr."
+last_name = "Barrette"
 
 # Create a variable full_name that 
 # joins first_name and last name.
-
-
+full_name = first_name + " " + last_name
+puts full_name
 
 # Now create a variable for age:
-
-age = 
+age = 21
 
 # Join your age to the end of your string
 # and print it to the screen.
-
+puts "Hello my name is #{full_name} and my age is #{age}"
+puts full_name + "," + age.to_s
 
 # What happens? Why?
