09ba0c6bbdde8fa41804d7745c7460643f6b67f2
Brandon Barrette <brandon.barrette@gmail.com>
2015-02-11 09:35:21 -0800

    add first lesson of operators

---
+++ ruby/lessons/4-Operators/1-operator_basics.rb
@@ -0,0 +1,27 @@
+# Assign a the current year to a variable 'year'
+
+
+# Increment the year by 1.
+
+
+# Increment the year by 5.
+
+
+# Decrease the year by 1.
+
+
+# Decrease the year by 5.
+
+
+# Assign your favorite number 
+# to the variable 'favorite_number'
+# Double your favorite number (and reassign it)
+
+# Divide your favorite number by 2 (and reassign it)
+
+
+# Find the remainder of your number when divided by 2.
+
+
+# Square your number (raise it to the exponenet of 2)
+
---
+++ ruby/lessons/Teacher Notes/4-Operators/1-operator_basics.rb
@@ -0,0 +1,41 @@
+# Assign a the current year to a variable 'year'
+year = 2015
+puts year
+
+# Increment the year by 1.
+year += 1
+puts year
+
+# Increment the year by 5.
+year +=5
+puts year
+
+# Decrease the year by 1.
+puts year -= 1
+
+# Decrease the year by 5.
+puts year -= 5
+
+# Assign your favorite number 
+# to the variable 'favorite_number'
+# Double your favorite number (and reassign it)
+favorite_number = 13
+puts "My favorite number: #{favorite_number}"
+
+favorite_number *= 2
+puts "My favorite number multiplied by 2: #{favorite_number}"
+
+# Divide your favorite number by 2 (and reassign it)
+favorite_number = 13
+favorite_number /= 2
+puts "My favorite number divided by 2: #{favorite_number}"
+
+# Find the remainder of your number when divided by 2.
+favorite_number = 13
+favorite_number %= 2
+puts "The remainder of my favorite number when divided by 2: #{favorite_number}"
+
+# Square your number (raise it to the exponenet of 2)
+favorite_number = 13
+favorite_number **= 2
+puts "The square of my favorite number: #{favorite_number}"
