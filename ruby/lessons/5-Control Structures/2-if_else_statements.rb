5efdc118328b069734fb364e9e96a07d60c69e84
Brandon Barrette <brandon.barrette@gmail.com>
2015-02-11 10:25:09 -0800

    add first two lessons for control structures

---
+++ ruby/lessons/5-Control Structures/2-if_else_statements.rb
@@ -0,0 +1,25 @@
+# If Else Statements
+
+# Reminder of what we did the last lesson.
+
+if car_speed < speed_limit
+  puts "Print an appropriate message"
+end
+
+if car_speed > speed_limit
+  puts "Print an appropriate message"
+end
+
+# Is this the most efficient way of doing things?
+# Notice the second statement is just the reverse
+# of the first, meaning, if the first is true, then
+# the second is false. If the first is false, then
+# the second is true. We can simplify this by saying:
+
+if car_speed < speed_limit
+  
+  
+end
+
+# Create your own if/else statement using the current
+# year and your graduation year as variables.
---
+++ ruby/lessons/Teacher Notes/5-Control Structures/3-else_if_statements.rb
@@ -0,0 +1,15 @@
+# Else If Statements
+
+# Reminder of what we did the last lesson.
+
+if car_speed < speed_limit
+  puts "Print an appropriate message"
+else
+  puts "Print an appropriate message"
+end
+
+
+
+
+# Create your own elsif statement using the current
+# year and your graduation year as variables.
