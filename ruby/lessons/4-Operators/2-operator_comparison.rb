0eed973a0311623d04528e8ee239d875b81b7116
Brandon Barrette <brandon.barrette@gmail.com>
2015-02-11 12:23:23 -0800

    add operators lessons for students

---
+++ ruby/lessons/4-Operators/2-operator_comparison.rb
@@ -0,0 +1,24 @@
+# Let's say we have the following two variables:
+
+a = 8
+b = 10
+c = 12
+
+# We can ask if they are equal by using ()
+
+
+
+# We can ask if they are not equal by using ()
+
+
+
+# We can ask for less than or greater than with (<, >)
+
+
+
+# And likewise for less than or equal to 
+# or greater than or equal to with (<=, >=)
+
+
+
+
---
+++ ruby/lessons/4-Operators/3-comparing_strings.rb
@@ -0,0 +1,17 @@
+# Comparing Strings
+
+# See what happens in the following comparisons:
+
+puts "a" > "b"
+puts "a" > "A"
+
+# In strings, each letter is given a corresponding
+# number, which you can find by calling the method
+# .ord
+
+
+
+# For a string of multiple letters, the comparison
+# is done for each letter in the string, until false
+# is arrived, otherwise true
+
