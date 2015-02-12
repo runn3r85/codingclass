6c930544d2c0f63b1abfcec8589f9f3f86f8de8c
Brandon Barrette <brandon.barrette@gmail.com>
2015-02-12 14:54:07 -0800

    add class notes for control structures lesson 1

--- ruby/lessons/5-Control Structures/1-if_statements.rb
+++ ruby/lessons/5-Control Structures/1-if_statements.rb
@@ -1,8 +1,9 @@
 # If statements
 
-# The basics of programming require us to ask questions
-# and based on true or false from those questions, we can
-# perform certain actions.
+# The basics of programming require us to
+# ask questions and based on true or false 
+# from those questions, we can perform 
+# certain actions.
 
 # For if statements we have:
 
@@ -12,16 +13,16 @@
 
 # Let's define speeds of a car and the speed limit:
 
-car_speed = 60
+car_speed = 45
 speed_limit = 50
 
 # Now let's compare the speed and speed limit
 # and print out the appropriate message
 
 if car_speed < speed_limit
-  puts "Print an appropriate message"
+  puts "You are driving safely."
 end
 
 if car_speed > speed_limit
-  puts "Print an appropriate message"
+  puts "You are going too fast for your own good."
 end
