595759bc968fc89a97ce609ad76059483c1ea723
Brandon Barrette <brandon.barrette@gmail.com>
2015-02-12 11:23:08 -0800

    add assignment 3

---
+++ ruby/assignments/assignment3.rb
@@ -0,0 +1,18 @@
+# Write a ruby program that performs the 
+# following actions:
+
+# First, it asks for a name and saves it 
+# to the variable name. It then prints
+# 'Hello name' to the screen.
+
+
+# Then it asks how you want to modify your name
+# by typing in 'uppercase' or 'reverse'.
+
+
+
+# Use a case statement to perform the method the user
+# requested (either uppercase or reverse). If the user 
+# types 'both', it should perform both actions. If a 
+# user types anything other than what is required, it
+# should return, 'Okay, maybe later.'
