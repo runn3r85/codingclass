aacc78febddb540af5f87a9830a351c5588566c6
Brandon Barrette <brandon.barrette@gmail.com>
2015-02-12 14:25:29 -0800

    add student notes for operators lesson 1

--- ruby/lessons/4-Operators/1-operator_basics.rb
+++ ruby/lessons/4-Operators/1-operator_basics.rb
@@ -1,27 +1,43 @@
 # Assign a the current year to a variable 'year'
-
+year = 2015
 
 # Increment the year by 1.
-
+year += 1
+puts "The year is #{year}"
 
 # Increment the year by 5.
-
+year += 5
+puts "The year is #{year}"
 
 # Decrease the year by 1.
-
+year -= 1
+puts "The year is #{year}"
 
 # Decrease the year by 5.
-
+year -= 5
+puts "The year is #{year}"
 
 # Assign your favorite number 
 # to the variable 'favorite_number'
 # Double your favorite number (and reassign it)
+favorite_number = 7
+puts "My favorite number is: #{favorite_number}"
+
+favorite_number *= 2
+puts "My favorite number doubled is: #{favorite_number}"
 
 # Divide your favorite number by 2 (and reassign it)
-
+favorite_number = 7
+favorite_number /= 2.0
+puts "My favorite number halved is: #{favorite_number}"
 
 # Find the remainder of your number when divided by 2.
-
+favorite_number = 7
+favorite_number %= 2
+puts "My favorite number modulo 2 is: #{favorite_number}"
 
 # Square your number (raise it to the exponenet of 2)
+favorite_number = 7
+favorite_number **= 2
+puts "My favorite number squared is: #{favorite_number}"
 
