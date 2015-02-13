cd897fd5b55927080fb6f8b91b7f4c05e8f8aa86
Brandon Barrette <brandon.barrette@gmail.com>
2015-02-13 15:26:03 -0800

    add lesson 2 of cntrol structures

--- ruby/lessons/5-Control Structures/2-if_else_statements.rb
+++ ruby/lessons/5-Control Structures/2-if_else_statements.rb
@@ -1,13 +1,14 @@
 # If Else Statements
+car_speed = 80
+speed_limit = 45
 
 # Reminder of what we did the last lesson.
-
 if car_speed < speed_limit
-  puts "Print an appropriate message"
+  puts "You are driving safely."
 end
 
 if car_speed > speed_limit
-  puts "Print an appropriate message"
+  puts "You are going too fast for your own good."
 end
 
 # Is this the most efficient way of doing things?
@@ -17,9 +18,35 @@ end
 # the second is true. We can simplify this by saying:
 
 if car_speed < speed_limit
-  
-  
+  puts "You are driving safely."
+else
+  puts "You are going too fast for your own good."
 end
 
-# Create your own if/else statement using the current
-# year and your graduation year as variables.
+# Create your own if/else statement using 
+# the current year and your graduation 
+# year as variables.
+
+current_year = 2015
+graduation_year = 2090
+
+if current_year == graduation_year
+  puts "Yeah i might grajumate."
+else
+  puts "not time yet."
+end
+
+if current_year > graduation_year
+  puts "i alredy grajumated."
+else
+  puts "not time yet."
+end
+
+if current_year < graduation_year
+  puts "not time yet."
+else
+  puts "i alredy grajumated."
+end
+
+
+
