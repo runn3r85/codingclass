c416ee6a811c853081add2042045a6223ea5f6db
Brandon Barrette <brandon.barrette@gmail.com>
2015-02-13 15:41:23 -0800

    finish lesson 3 class notes from control structures

--- ruby/lessons/5-Control Structures/3-else_if_statements.rb
+++ ruby/lessons/5-Control Structures/3-else_if_statements.rb
@@ -1,6 +1,8 @@
 # Else If Statements
 
 # Reminder of what we did the last lesson.
+car_speed = 45
+speed_limit = 45
 
 if car_speed < speed_limit
   puts "You are under the speed limit!"
@@ -11,7 +13,37 @@ end
 # What happens if the car_speed is equal to 
 # the speed_limit? How can we account for that?
 
+if car_speed < speed_limit
+  puts "You are under the speed limit!"
+elsif car_speed == speed_limit
+  puts "You are going the speed limit!"
+else
+  puts "You are speeding! Slow down!"
+end
+
+# Create your own elsif statement using 
+# the current year and your graduation 
+# year as variables.
+
+current_year = 2015
+graduation_year = 2016
+
+if graduation_year > current_year
+  puts "Did you do it?"
+elsif graduation_year == current_year
+  puts "You ready...."
+else
+  puts "You already graduated."
+end
 
 
-# Create your own elsif statement using the current
-# year and your graduation year as variables.
+
+
+
+
+
+
+
+
+
+
