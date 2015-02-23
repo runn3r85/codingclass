b8918464ef722fc2eb9040ef4d94d7cd5f59e71f
Brandon Barrette <brandon.barrette@gmail.com>
2015-02-23 15:16:31 -0800

    add last lesson of Control Structures

--- ruby/lessons/5-Control Structures/4-case_statements.rb
+++ ruby/lessons/5-Control Structures/4-case_statements.rb
@@ -4,23 +4,47 @@
 # multiple values and don't really need to write a complex
 # if/elsif statements. For example:
 
-var color = "blue";
+# print "What's your favorite color? "
+# color = gets.chomp.downcase
 
-if color == "blue"
-  puts "blue"
-elsif color == "red"
-  puts "red"
-elsif color == "yellow"
-  puts "yellow"
-elsif color == "green"
-  puts "green"
-else
-  puts "black"
-end
+# if color == "blue"
+#   puts "blue"
+# elsif color == "red"
+#   puts "red"
+# elsif color == "yellow"
+#   puts "yellow"
+# elsif color == "green"
+#   puts "green"
+# else
+#   puts "black"
+# end
 
 # This can be tedious to do and it's a huge waste of 
 # typing here. We can say that there is a 'case' called
 # color that has many possible choices. Here's what 
 # this would look like as a case statment:
 
+case color
+when "blue"
+  puts "That's the color of the sky!"
+when "red"
+  puts "Rubies are red!!!!!!!!"
+when "green"
+  puts "That's my favorite color!"
+when "yellow"
+  puts "That's a lame favorite color"
+else
+  puts "That's not a color."
+end
 
+# FROM FRIDAYS CODE
+
+print "What's your graduation year? "
+gets graduation_year.chomp
+
+case graduation_year
+when Time.now.year
+  puts "You ready...."
+else
+  puts "You don't graduate this year."
+end
