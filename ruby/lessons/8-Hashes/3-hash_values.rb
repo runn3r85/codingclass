be50f79dc6f50c87b69ea1978978d6d6b2105cfb
Brandon Barrette <brandon.barrette@gmail.com>
2015-03-02 15:27:01 -0800

    add lesson 3 for hashes

--- ruby/lessons/8-Hashes/3-hash_values.rb
+++ ruby/lessons/8-Hashes/3-hash_values.rb
@@ -10,23 +10,25 @@ puts hash.inspect
 # To return an array of the values in the hash, 
 # we can use the values method:
 
-puts hash.values
-# => 
+puts hash.values.inspect
+# => ["apple", 1, "Gala"]
 
 # The has_value? method takes one argument 
 # and returns true or false if the value 
 # is contained within the hash:
 
-
+puts hash.has_value?("Gala")    # => true
+puts hash.has_value?("type")    # => false
 
 # That would return false since "type" isn't a value 
 # (remember it's a key). However, the following would return true:
 
-
+puts hash.has_value?(1)   # => true
 
 # The values_at method takes several arguments 
 # and returns the hash values at the specified 
 # keys as an array:
+# .values_at?(key1, key2, key3, ...)
 
-
+puts hash.values_at("quantity", "type").inspect
 # => [1, "Gala"]
