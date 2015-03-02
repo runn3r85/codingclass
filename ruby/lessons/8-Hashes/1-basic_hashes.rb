748d996f5e707ed6d86276f11d7d7b895c5ca423
Brandon Barrette <brandon.barrette@gmail.com>
2015-03-02 15:05:49 -0800

    add class notes for hashes lesson 1

--- ruby/lessons/8-Hashes/1-basic_hashes.rb
+++ ruby/lessons/8-Hashes/1-basic_hashes.rb
@@ -2,15 +2,20 @@
 # Basic Hashes
 # =========================
 
+# Hashes have keys, which are similar to
+# index in arrays.
+
 # To create a hash, we instantiate a new 
 # instance of the Hash class:
 
-
+item = Hash.new()
+puts item.inspect
 
 # This is identical to how we built arrays.
 # Hashes can also be created by using curly braces:
 
-
+item = { "item" => "apple", "quantity" => 1 }
+puts item.inspect
 
 # Notice this is similar to arrays with brackets []
 # The main difference between an array and hash is that
@@ -20,14 +25,15 @@
 # When creating a hash using curly braces, 
 # keys and values can also be specified:
 
-
-
-
+item = { item: "apple", quantity: 1 }
+# :item => "apple" the same as item: "apple"
+puts item.inspect
 
 # Hash keys can be almost any Ruby type. 
 # Here's an example of using symbols as hash keys:
 
-
+item[1] = "Grocery Store"
+puts item.inspect
 
 # Hash keys can also be numbers. 
 # If we set the following hash key and value:
@@ -36,7 +42,7 @@
 
 # Inspect our hash and we would see:
 
-puts item.inspect
+# puts item.inspect
 # { :item => "Bread", :quantity => 1, 1 => "Grocery Store" }
 
 # Once a hash has been instantiated, it is possible 
@@ -44,3 +50,5 @@ puts item.inspect
 # of the hash, brackets containing the new key, an equals sign, 
 # and the new value for said key:
 
+item["type"] = "Fiji"
+puts item.inspect
