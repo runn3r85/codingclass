db9e221756d95914c408aeb62b13b1a41112d9a7
Brandon Barrette <brandon.barrette@gmail.com>
2015-03-02 11:58:22 -0800

    add files for future lessons

---
+++ ruby/lessons/Teacher Notes/projects/hash_practice/step3.rb
@@ -0,0 +1,37 @@
+def create_list
+  print "What is the list name? "
+  name = gets.chomp
+
+  hash = { "name" => name, "items" => Array.new }
+  return hash
+end
+
+def add_list_item
+  print "What is the item called? "
+  item_name = gets.chomp
+
+  print "How much? "
+  quantity = gets.chomp.to_i
+
+  hash = { "name" => item_name, "quantity" => quantity }
+  return hash
+end
+
+def print_list(list)
+  puts "List: #{list['name']}"
+  puts "----"
+
+  list["items"].each do |item|
+    puts "Item: " + item['name']
+    puts "Quantity: " + item['quantity'].to_s
+    puts "---"
+  end
+end
+
+list = create_list()
+puts list.inspect
+list['items'].push(add_list_item())
+
+puts list.inspect
+
+print_list(list)
