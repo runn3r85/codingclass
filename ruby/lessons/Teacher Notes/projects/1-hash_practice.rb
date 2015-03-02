190e541d06d42928b2fb5dbeccce951c34e36d79
Brandon Barrette <brandon.barrette@gmail.com>
2015-03-02 11:50:06 -0800

    add sample project for hash practice

---
+++ ruby/lessons/Teacher Notes/projects/1-hash_practice.rb
@@ -0,0 +1,57 @@
+# =========================
+# Hash Practice
+# =========================
+
+# Create a method called create_list that asks
+# the user for the list name and creates and
+# returns a new hash to store the name.
+
+def create_list
+  print "What is the list name? "
+  name = gets.chomp
+
+  hash = { "name" => name, "items" => Array.new }
+  return hash
+end
+
+# Create a method called add_list_item that asks
+# the user for the item and creates and
+# returns a new hash to store the item.
+
+def add_list_item
+  print "What is the item called? "
+  item_name = gets.chomp
+
+  print "How much? "
+  quantity = gets.chomp.to_i
+
+  hash = { "name" => item_name, "quantity" => quantity }
+  return hash
+end
+
+def print_separator(character="-")
+  puts character * 80
+end
+
+def print_list(list)
+  puts "List: #{list['name']}"
+  print_separator()
+
+  list["items"].each do |item|
+    puts "\tItem: " + item['name'] + "\t\t\t" +
+         "Quantity: " + item['quantity'].to_s
+  end
+
+  print_separator()
+end
+
+list = create_list()
+
+puts "Great! Add some items to your list."
+
+list['items'].push(add_list_item())
+list['items'].push(add_list_item())
+list['items'].push(add_list_item())
+
+puts "Here's your list:\n"
+print_list(list)    
---
+++ ruby/projects/1-hash_practice.rb
@@ -0,0 +1,7 @@
+# =========================
+# Hash Practice
+# =========================
+
+# Create a method called create_list that asks
+# the user for the list name and creates and
+# returns a new hash to store the name.
