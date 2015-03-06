21dec0d10abb8499d25386385287e6ac316c9af1
Brandon Barrette <brandon.barrette@gmail.com>
2015-03-06 15:29:18 -0800

    add hash iteration lesson

--- ruby/lessons/9-Loops/6-hash_iteration.rb
+++ ruby/lessons/9-Loops/6-hash_iteration.rb
@@ -11,19 +11,36 @@ hash = { "item" => "apple", "quantity" => 1, "type" => "Gala" }
 # We separate arguments to blocks using a comma, 
 # just like method arguments:
 
+hash.each do |key, value|
+  puts "The key is #{key} and value #{value}"
+end
 
 # We can do this inline too:
 
-
+hash.each { |key,value| puts "The key is #{key} and value #{value}" }
 
 # We can also use .each_pair to represent .each on hashes
 
-
+hash.each_pair do |key, value|
+  puts "The key is #{key} and value #{value}"
+end
 
 # If we want to just iterate over the keys, we can use:
 
-
+hash.each_key do |key|
+  puts "The key is: #{key}"
+end
 
 # Likewise to iterate over the values, we can use:
+
+hash.each_value do |value|
+  puts "The value is: #{value}"
+end
+
+
+
+
+
+
 
 
