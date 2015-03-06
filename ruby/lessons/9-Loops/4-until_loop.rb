60b244ddf0e16f7f99c67b78b0502e8192657e71
Brandon Barrette <brandon.barrette@gmail.com>
2015-03-06 15:10:59 -0800

    add class notes for lesson 4

--- ruby/lessons/9-Loops/4-until_loop.rb
+++ ruby/lessons/9-Loops/4-until_loop.rb
@@ -1,12 +1,22 @@
 # =========================
-# Unit Loops
+# Until Loops
 # =========================
 
 # Define an until loop that asks the user
 # if they want the loop to continue by
 # entering y/n.
 
+# answer = ""
+# while answer != "n" 
+#   print "Do you want to continue? (y/n)"
+#   answer = gets.chomp.downcase
+# end
 
+answer = ""
+until answer == "n"
+  print "Do you want to continue? (y/n)"
+  answer = gets.chomp.downcase
+end
 
 # Here is the print_hello method from the while loop.
 
@@ -21,4 +31,11 @@ end
 # Use an until loop that asks the user 
 # how many times they want to print hello
 # and exits if they enter a number greater than 5.
+
+answer = 0
+until answer >= 5 
+  print "How many times do you want to print 'hello'? Enter a number greater than 5 to exit."
+  answer = gets.chomp.to_i
+  print_hello(answer)
+end
 
