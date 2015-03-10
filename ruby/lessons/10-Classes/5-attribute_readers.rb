6350b3ac268c791a4762a2d1f7a3f955d4b15136
Brandon Barrette <brandon.barrette@gmail.com>
2015-03-10 14:15:52 -0700

    add lesson 5 to classes

--- ruby/lessons/10-Classes/5-attribute_readers.rb
+++ ruby/lessons/10-Classes/5-attribute_readers.rb
@@ -6,25 +6,44 @@
 # which simplifies the methods required to be defined.
 
 class Name
-  def initialize(title, first_name)
+  attr_reader :title, :first_name, :middle_name, :last_name
+
+  def initialize(title, first_name, middle_name, last_name)
     @title = title
     @first_name = first_name
+    @middle_name = middle_name
+    @last_name = last_name
   end
 
-  def title
-    @title
+  def full_name
+    first_name + " " + middle_name + " " + last_name
   end
 
-  def first_name
-    @first_name
+  def full_name_with_title
+    title + " " + full_name
   end
 
-  def middle_name
-    "M."
+  def first_name=(value)
+    @first_name = value
   end
 
-  def last_name
-    "Mouse"
+  def last_name=(value)
+    @last_name = value
   end
 end
+
+mickey = Name.new("Mr.", "Mickey","M.", "Mouse")
+puts mickey.full_name
+puts mickey.full_name_with_title
+mickey.first_name = "Donald"
+puts mickey.inspect
+puts mickey.full_name
+mickey.last_name = "Duck"
+puts mickey.inspect
+puts mickey.full_name
+
+
+
+
+
 
