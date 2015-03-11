080a031fd7faa48562df2ccfc8eee93ff1ca3acd
Brandon Barrette <brandon.barrette@gmail.com>
2015-03-11 12:23:15 -0700

    add bank account project

---
+++ ruby/lessons/Teacher Notes/projects/bank_account/step3.rb
@@ -0,0 +1,38 @@
+class BankAccount
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @transactions = []
+    add_transaction("Beginning Balance", 0)
+  end
+
+  def credit(description, amount)
+    add_transaction(description, amount)
+  end
+
+  def debit(description, amount)
+    add_transaction(description, -amount)
+  end
+
+  def add_transaction(description, amount)
+    @transactions.push(description: description, amount: amount)
+  end
+
+  def balance
+    balance = 0.0
+    @transactions.each do |transaction|
+      balance += transaction[:amount]
+    end
+    return balance
+  end
+
+  def to_s
+    "Name: #{name}, Balance: #{sprintf("%0.2f", balance)}"
+  end
+end
+
+bank_account = BankAccount.new("Jason")
+bank_account.credit("Paycheck", 100)
+bank_account.debit("Groceries", 40)
+puts bank_account
