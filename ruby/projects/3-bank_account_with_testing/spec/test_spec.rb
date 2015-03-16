8c977a4b6d654d26378798fe1b6f24878b9c0090
Brandon Barrette <brandon.barrette@gmail.com>
2015-03-16 14:32:27 -0700

    add gem install line to test_spec file

--- ruby/projects/3-bank_account_with_testing/spec/test_spec.rb
+++ ruby/projects/3-bank_account_with_testing/spec/test_spec.rb
@@ -5,5 +5,8 @@ Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 $:.unshift File.expand_path(File.dirname(__FILE__) + '/..')
 
+# Need to run:
+# gem install minitest-reporters
+
 # Helpful link:
 # http://www.mattsears.com/articles/2011/12/10/minitest-quick-reference
