b880774a9b4cc9f9c2bd4dc3809370a78986eb49
Brandon Barrette <brandon.barrette@gmail.com>
2015-03-16 12:19:01 -0700

    add link to minitest::spec quick guide

--- ruby/projects/3-bank_account_with_testing/spec/test_spec.rb
+++ ruby/projects/3-bank_account_with_testing/spec/test_spec.rb
@@ -4,3 +4,6 @@ require "minitest/reporters"
 Minitest::Reporters.use! Minitest::Reporters::SpecReporter.new
 
 $:.unshift File.expand_path(File.dirname(__FILE__) + '/..')
+
+# Helpful link:
+# http://www.mattsears.com/articles/2011/12/10/minitest-quick-reference
