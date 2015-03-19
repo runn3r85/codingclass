b46d2931744fd02f49b2ecfb25dd7f54649a2ffe
Brandon Barrette <brandon.barrette@gmail.com>
2015-03-19 15:02:05 -0700

    Add assignment 7

---
+++ ruby/assignments/assignment7.rb
@@ -0,0 +1,16 @@
+# Due Friday March 27th 
+
+# Build a contacts app using THREE classes 
+# just like assignment 6. HOWEVER, you must
+# have tests for each of the classes.
+
+# The file structure should look like:
+# Folder: assignment7
+#   > address_book.rb
+#   > contact.rb
+#   > number.rb
+#   Folder: spec
+#     > address_book_spec.rb
+#     > contact_spec.rb
+#     > number_spec.rb
+#     > test_spec.rb (copied from project 3)
